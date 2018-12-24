a0f38f91493538ee64b6b21ce410e23e1aaa9f9d
khturekni <lior.kalman@outlook.com>
2018-12-24 13:35:28 +0200

    added basic implementation for annotation

--- OOP/Solution/OOPExpectedExceptionImpl.java
+++ OOP/Solution/OOPExpectedExceptionImpl.java
@@ -1,4 +1,28 @@
 package OOP.Solution;
 
-public class OOPExpectedExceptionImpl {
+import OOP.Provided.OOPExpectedException;
+
+public class OOPExpectedExceptionImpl implements OOPExpectedException{
+
+    public Class<? extends Exception> getExpectedException(){
+
+    }
+
+    public OOPExpectedException expect(Class<? extends Exception> expected){
+
+    }
+
+    public OOPExpectedException expectMessage(String msg){
+
+    }
+
+    public boolean assertExpected(Exception e){
+
+    }
+
+    static OOPExpectedException none() {
+
+        return OOPExpectedExceptionImpl.none();
+    }
+
 }
---
+++ OOP/Solution/OOPTestClass.java
@@ -0,0 +1,15 @@
+package OOP.Solution;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface OOPTestClass {
+    enum OOPTestClassType{
+        ORDERED, UNORDERED
+    }
+    OOPTestClassType value() default OOPTestClassType.UNORDERED;
+}
